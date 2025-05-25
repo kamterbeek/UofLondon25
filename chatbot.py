9d51ca1d51f10c6d6672f4ca59df71f3bf455af8
Kassandra <kam.terbeek@gmail.com>
2025-05-24 19:54:04 -0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,29 @@
 
+questions = ["hello", "carbon", "temperature", "climate", "sustainability"]
+responses = [
+    "What would you like to learn about today?",
+    "Sure! Are you interested in the carbon emissions of a transatlantic flight?",
+    "The temperature of the ocean is rising at an averate rate of 3.3 mm a year.",
+    "To battle the climate crisis, as a citizen, you can eat less meat, drive less, and vote and advocate for policy.",
+    "You can create a more sustainable lifestyle by consuming and shopping less, updating your home for longer lasting sustainable features."
+    "You are welcome.",
+    "Goodbye! Have a nice day."
+]
+​
+def chatbot():
+    print("Welcome to the climate aware chatbot!")
+    print("What would you like to learn about today?")
+​
+    # Main loop:
+while True:
+    user_input = input("You: ").lower()
+    if user_input in questions:
+        index = questions.index(user_input)
+        print("Bot: ", responses[index])
+        if user_input == "bye":
+            break;
+            
+    else:
+        print("Bot: I am not sure how to respond to that.")
+​
+chatbot()
